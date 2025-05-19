b0808ea9e0276fa08143ee14dd310549cd39484c
TanNV <vantankmt@gmail.com>
2025-05-19 16:31:49 +0700

    feat: setup icon for bottom navigation

--- src/screens/BottomNavigation/BottomNavigation.tsx
+++ src/screens/BottomNavigation/BottomNavigation.tsx
@@ -1,7 +1,6 @@
 // LIB
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // IMPORT
 import HomeScreen from '../Home/HomeScreen';
@@ -9,23 +8,58 @@ import ExploreScreen from '../Explore/ExploreScreen';
 import MyListScreen from '../MyList/HomeScreen';
 import DownloadScreen from '../Downloads/DownloadScreen';
 import ProfileScreen from '../Profile/ProfileScreen';
+import HomeIcon from '@assets/icons/ic_tab_home.svg';
+import SelectedHomeIcon from '@assets/icons/ic_tab_selected_home.svg';
+import ExploreIcon from '@assets/icons/ic_tab_explore.svg';
+import SelectedExploreIcon from '@assets/icons/ic_tab_selected_explore.svg';
+import MyListIcon from '@assets/icons/ic_tab_my_list.svg';
+import SelectedMyListIcon from '@assets/icons/ic_tab_selected_my_list.svg';
+import DownloadIcon from '@assets/icons/ic_tab_download.svg';
+import SelectedDownloadIcon from '@assets/icons/ic_tab_selected_download.svg';
+import ProfileIcon from '@assets/icons/ic_tab_profile.svg';
+import SelectedProfileIcon from '@assets/icons/ic_tab_selected_profile.svg';
 
 const Tab = createBottomTabNavigator();
+
+type TabRouteName = 'Home' | 'Explore' | 'My List' | 'Download' | 'Profile';
+
+const iconMap: Record<
+  TabRouteName,
+  {active: React.ReactNode; inactive: React.ReactNode}
+> = {
+  Home: {
+    active: <SelectedHomeIcon />,
+    inactive: <HomeIcon />,
+  },
+  Explore: {
+    active: <SelectedExploreIcon />,
+    inactive: <ExploreIcon />,
+  },
+  'My List': {
+    active: <SelectedMyListIcon />,
+    inactive: <MyListIcon />,
+  },
+  Download: {
+    active: <SelectedDownloadIcon />,
+    inactive: <DownloadIcon />,
+  },
+  Profile: {
+    active: <SelectedProfileIcon />,
+    inactive: <ProfileIcon />,
+  },
+};
 
 const BottomTabs = () => {
   return (
     <Tab.Navigator
       screenOptions={({route}) => ({
-        tabBarIcon: ({color, size}) => {
-          let iconName = 'home';
-
-          if (route.name === 'Home') iconName = 'home';
-          else if (route.name === 'Profile') iconName = 'person';
-          else if (route.name === 'Settings') iconName = 'settings';
-
-          return <Ionicons name={iconName} size={size} color={color} />;
+        tabBarIcon: ({color, size, focused}) => {
+          const iconName = focused
+            ? iconMap[route.name as TabRouteName].active
+            : iconMap[route.name as TabRouteName].inactive;
+          return iconName;
         },
-        tabBarActiveTintColor: '#6200ee',
+        tabBarActiveTintColor: '#E21221',
         tabBarInactiveTintColor: 'gray',
       })}>
       <Tab.Screen name="Home" component={HomeScreen} />
