6a4fe50f4b9686d72a322cfdafab59be2b293bca
TanNV <vantankmt@gmail.com>
2025-06-05 10:37:18 +0700

    feat: create ui explore screen

--- src/screens/Explore/ExploreScreen.tsx
+++ src/screens/Explore/ExploreScreen.tsx
@@ -1,12 +1,86 @@
 // LIB
-import {Text, View} from 'react-native';
+import {useCallback, useEffect} from 'react';
+import {Dimensions, FlatList, ScrollView, Text, View} from 'react-native';
+import {ScaledSheet} from 'react-native-size-matters';
+import {useDispatch, useSelector} from 'react-redux';
+
+// IMPORT
+import CustomHeader from '@/components/common/CustomHeader';
+import {AppDispatch, RootState} from '@/redux/store';
+import {fetchTrendingVideos} from '@/redux/Slice/ExploreSlice';
+import {MovieDetailModel} from '@/models/homeModels';
+import ItemMovie from '@/components/common/ItemMovie';
+
+const {width} = Dimensions.get('window');
+const ITEM_WIDTH = (width - 50) / 2;
 
 const ExploreScreen = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const {isLoading, exploreMovies} = useSelector(
+    (state: RootState) => state.explore,
+  );
+
+  useEffect(() => {
+    dispatch(fetchTrendingVideos());
+  }, []);
+
+  const renderItem = useCallback(
+    ({item, index}: {item: MovieDetailModel; index: number}) => {
+      const itemStyle =
+        index % 2 === 0
+          ? [styles.itemContainer, styles.itemMarginRight]
+          : [styles.itemContainer, styles.itemMarginLeft];
+
+      return (
+        <View style={itemStyle}>
+          <ItemMovie movie={item} width={ITEM_WIDTH} />
+        </View>
+      );
+    },
+    [],
+  );
+
   return (
-    <View>
-      <Text>ExploreScreen</Text>
+    <View style={styles.container}>
+      <CustomHeader title="Explore" textProp={{alignItems: 'center'}} />
+      <FlatList
+        data={exploreMovies}
+        numColumns={2}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContent}
+        style={styles.listStyle}
+        showsVerticalScrollIndicator={false}
+        // 🔥 Performance Props
+        removeClippedSubviews={true}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        windowSize={7}
+      />
     </View>
   );
 };
+
+const styles = ScaledSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 30,
+  },
+  itemContainer: {
+    marginBottom: 16,
+  },
+  itemMarginRight: {
+    marginEnd: 8,
+  },
+  itemMarginLeft: {
+    marginStart: 8,
+  },
+  listStyle: {marginTop: 10, alignSelf: 'center'},
+  listContent: {
+    paddingVertical: 10,
+    marginTop: 10,
+  },
+});
 
 export default ExploreScreen;
--- src/screens/WatchTrailers/WatchTrailersScreen.tsx
+++ src/screens/WatchTrailers/WatchTrailersScreen.tsx
@@ -1,5 +1,5 @@
 // LIB
-import {Alert, Button, SafeAreaView, View} from 'react-native';
+import {View} from 'react-native';
 import {useSelector} from 'react-redux';
 import {moderateScale, ScaledSheet} from 'react-native-size-matters';
 import YoutubePlayer from 'react-native-youtube-iframe';
@@ -7,47 +7,21 @@ import YoutubePlayer from 'react-native-youtube-iframe';
 // IMPORT
 import {RootState} from '@/redux/store';
 import CustomHeader from '@/components/common/CustomHeader';
-import {useCallback, useState} from 'react';
 
 const WatchTrailersScreen = () => {
   const {videos} = useSelector((state: RootState) => state.video);
-  const [playing, setPlaying] = useState(false);
-
-  const onStateChange = useCallback((state: string) => {
-    if (state === 'ended') {
-      setPlaying(false);
-      Alert.alert('video has finished playing!');
-    }
-  }, []);
-
-  const togglePlaying = useCallback(() => {
-    setPlaying(prev => !prev);
-  }, []);
 
   return (
     <View style={styles.container}>
-      <YoutubePlayer
-        height={300}
-        play={playing}
-        videoId={videos[0].key}
-        onChangeState={onStateChange}
+      <CustomHeader
+        title={videos[0].name ?? ''}
+        textProp={{paddingStart: moderateScale(30)}}
       />
-      <Button title={playing ? 'pause' : 'play'} onPress={togglePlaying} />
+      {videos.length > 0 && (
+        <YoutubePlayer height={300} play={true} videoId={videos[0].key} />
+      )}
     </View>
   );
-  //   return (
-  //     <View style={styles.container}>
-  //       <CustomHeader
-  //         title={videos[0].name ?? ''}
-  //         textProp={{paddingStart: moderateScale(30)}}
-  //       />
-  //       {videos.length > 0 && (
-  //         <View style={{backgroundColor: 'blue'}}>
-  //           <YoutubePlayer height={300} play={true} videoId={videos[0].key} />
-  //         </View>
-  //       )}
-  //     </View>
-  //   );
 };
 
 const styles = ScaledSheet.create({
