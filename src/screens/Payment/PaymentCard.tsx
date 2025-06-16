1357af269c753d1c53da5d741905ec5a1332beb8
TanNV <vantankmt@gmail.com>
2025-06-16 08:53:38 +0700

    feat: logic payment

--- src/screens/Payment/PaymentCard.tsx
+++ src/screens/Payment/PaymentCard.tsx
@@ -6,72 +6,134 @@ import {
   CardField,
   confirmPayment,
   createPaymentMethod,
+  handleNextAction,
 } from '@stripe/stripe-react-native';
 import {Details} from '@stripe/stripe-react-native/lib/typescript/src/types/components/CardFieldInput';
+import {Result} from '@stripe/stripe-react-native/lib/typescript/src/types/PaymentMethod';
 
 // IMPORT
 import CustomButton from '@/components/common/CustomButton';
+import {isNullOrEmpty} from '@/utils/checkEmpty';
 
 const PaymentCard = () => {
   const [cardDetails, setCardDetails] = useState<Details>();
 
-  const handlePayPress = async () => {
-    if (!cardDetails?.complete) {
-      Alert.alert('Error', 'Please enter complete card details.');
-      return;
-    }
-
-    const {paymentMethod, error} = await createPaymentMethod({
-      paymentMethodType: 'Card',
-      paymentMethodData: {
-        billingDetails: {
-          email: 'email@stripe.com',
-          phone: '+48888000888',
-          address: {
-            city: 'Houston',
-            country: 'US',
-            line1: '1459  Circle Drive',
-            line2: '',
-            postalCode: '77063',
-            state: 'Texas',
+  const onCreatePaymentMethod = async () => {
+    try {
+      const {paymentMethod, error} = await createPaymentMethod({
+        paymentMethodType: 'Card',
+        paymentMethodData: {
+          billingDetails: {
+            email: 'email@stripe.com',
+            phone: '+48888000888',
+            address: {
+              city: 'Houston',
+              country: 'US',
+              line1: '1459  Circle Drive',
+              line2: '',
+              postalCode: '77063',
+              state: 'Texas',
+            },
           },
         },
-      },
-    });
+      });
 
-    if (error) {
-      Alert.alert('Error', error.message);
-    } else {
-      console.log('PaymentMethod ID:', paymentMethod.id);
+      return {
+        paymentMethod,
+        error,
+      };
+    } catch (error) {
+      throw error;
     }
+  };
 
-    console.log('----1111 ', paymentMethod);
-    // const response = await axios.post(
-    //   'https://your-backend.com/create-payment-intent',
-    //   {
-    //     amount: 500,
-    //   },
-    //   {
-    //     headers: {'Content-Type': 'application/json'},
-    //   },
-    // );
+  const onCreatePaymentIntent = async ({
+    amount,
+    paymentMethod,
+  }: {
+    amount: number;
+    paymentMethod: Result;
+  }) => {
+    try {
+      const response = await axios.post(
+        'http://10.86.142.57:3000/payment/create-payment-intent',
+        {
+          amount: amount,
+          payment_method_id: paymentMethod.id,
+        },
+        {
+          headers: {'Content-Type': 'application/json'},
+        },
+      );
+      const {clientSecret, status, requiresAction} = response.data;
 
-    // const {clientSecret} = await response.data;
+      return {
+        clientSecret,
+        status,
+        requiresAction,
+      };
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    // const {paymentIntent, error} = await confirmPayment(clientSecret, {
-    //   paymentMethodType: 'Card',
-    //   paymentMethodData: {
-    //     billingDetails: {
-    //       name: 'Test User',
-    //     },
-    //   },
-    // });
+  const handlePayPress = async () => {
+    try {
+      // Check card details complete
+      if (!cardDetails?.complete) {
+        Alert.alert('Error', 'Please enter complete card details!');
+        return;
+      }
 
-    // if (error) {
-    //   console.log('Payment failed:', error);
-    // } else if (paymentIntent) {
-    //   console.log('Payment succeeded:', paymentIntent);
-    // }
+      const {paymentMethod, error} = await onCreatePaymentMethod();
+
+      if (error || !paymentMethod) {
+        Alert.alert('Error when create payment method', error?.message);
+        return;
+      }
+
+      const {clientSecret, status, requiresAction} =
+        await onCreatePaymentIntent({
+          amount: 500,
+          paymentMethod: paymentMethod,
+        });
+
+      // Check if payment requires action
+      if (!isNullOrEmpty(clientSecret) && requiresAction !== true) {
+        Alert.alert('Success!: The payment was confirmed successfully!');
+        return;
+      }
+
+      // Handle if payment requires action
+      const paymentIntent = await handleNextAction(clientSecret);
+
+      if (!paymentIntent.paymentIntent?.id) {
+        Alert.alert('Handle next action faild!');
+        return;
+      }
+
+      const paymentData = await confirmPayment(paymentIntent.paymentIntent.id);
+
+      if (paymentData.error) {
+        console.log('Payment failed:', paymentData.error);
+      } else if (paymentData.paymentIntent) {
+        console.log('Payment succeeded:', paymentData.paymentIntent);
+      }
+    } catch (err) {
+      let errorMessage = 'Something went wrong';
+
+      if (axios.isAxiosError(err)) {
+        if (err.response?.data?.message) {
+          errorMessage = err.response.data.message;
+        } else {
+          errorMessage = err.message;
+        }
+      } else if (err instanceof Error) {
+        errorMessage = err.message;
+      }
+
+      Alert.alert('Payment failed: ', errorMessage);
+    }
   };
 
   return (
